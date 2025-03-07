ae85b5e76178cf9057fc7d6ebf2f05406de55372
KJane0120 <rlawoals5755@gmail.com>
2025-03-07 15:09:15 +0900

    [Feat]점프대 구현
    jumpValue 250으로 설정했습니다.

---
+++ Assets/Scripts/Platform/JumpPlatform.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlatform : MonoBehaviour
+{
+
+    [SerializeField] private float jumpValue;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+                rb.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+
+            }
+        }
+    }
+}
